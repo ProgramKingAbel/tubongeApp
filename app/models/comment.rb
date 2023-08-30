422a92e64c61ffea5de9899741617c8d79ebcc55
ProgramKingAbel <abelmorara254@gmail.com>
2023-08-30 18:56:23 +0300

    Run callback increase counters on save

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,8 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
+  after_save :increase_comment_counter
+
   def increase_comment_counter
     post.increment!(:comments_counter)
   end
