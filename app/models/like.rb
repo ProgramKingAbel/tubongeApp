9b2033250d9382064a3266e495e2fe6e81df25d5
ProgramKingAbel <abelmorara254@gmail.com>
2023-08-30 17:17:38 +0300

    Test custom methods

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,7 +2,7 @@ class Like < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
-  def increment_likes_counter
+  def update_likes_counter
     post.increment!(:likes_counter)
   end
 end
