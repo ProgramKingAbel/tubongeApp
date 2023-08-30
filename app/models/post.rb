eeed4497f266128e57f7c29cbbf6f5a92637c634
ProgramKingAbel <abelmorara254@gmail.com>
2023-08-30 16:46:55 +0300

    add custom methods for posts counter increment and top 5 comments @post

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
+
+  def update_author_posts_counter
+    author.increment!(:posts_counter)
+  end
+
+  def top_five_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
