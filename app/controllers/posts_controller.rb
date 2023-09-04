a2cbce328b73c3d522dfd3f93c96cf7ab27b6aca
ProgramKingAbel <abelmorara254@gmail.com>
2023-09-04 20:13:22 +0300

    Fix PostController show Action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,16 @@
 class PostsController < ApplicationController
+  before_action :find_user
+
   def index
-    @user = User.find(params[:id])
-    @user_posts = @user.posts.all
+    @user_posts = @user.posts
   end
 
   def show
-    @user_post = @user.posts.find[:id])
+    @user_post = @user.posts.find_by(id: params[:id])                                      
+  end
+
+  private
+  def find_user
+    @user = User.find(params[:user_id])
   end
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  resources :users do
+  resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
   end
 
