02639e2734ad85e463faa80dadf2de76dc3e1717
ProgramKingAbel <abelmorara254@gmail.com>
2023-09-04 20:33:02 +0300

    Fix rubocop linters:

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,11 +6,12 @@ class PostsController < ApplicationController
   end
 
   def show
-    @user_post = @user.posts.find_by(id: params[:id])                                      
+    @user_post = @user.posts.find_by(id: params[:id])
   end
 
   private
+
   def find_user
     @user = User.find(params[:user_id])
   end
-end 
+end
