f18f7d65d86fe5186d20404e870722d6d9cfdadc
ProgramKingAbel <abelmorara254@gmail.com>
2023-09-04 18:36:04 +0300

    Implement nested routing for posts-users

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  resources :users, :posts
+  resources :users do
+    resources :posts, only: [:index, :show]
+  end
 
   root 'users#index'
 
