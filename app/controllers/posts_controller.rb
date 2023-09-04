bc70ba4d89b70e144aaa74d6f086146e9c0fb95b
ProgramKingAbel <abelmorara254@gmail.com>
2023-09-04 18:44:49 +0300

    Implement index and show actions for posts ontroller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,10 @@
 class PostsController < ApplicationController
   def index
+    @user = User.find(params[:id])
+    @user_posts = @user.posts.all
   end
 
   def show
+    @user_post = @user.posts.find[:id])
   end
 end 
