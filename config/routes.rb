ac76fcebfda98c98448daa1049eec38d1f911b23
ProgramKingAbel <abelmorara254@gmail.com>
2023-09-06 16:45:08 +0300

    Define custom route for show all

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
+    get 'show_all', on: :member
   end
 
   root 'users#index'
