eb0868d9b78f13de205bfbab241cd64ec1b4b71d
ProgramKingAbel <abelmorara254@gmail.com>
2023-09-07 03:33:42 +0300

    Fix linters

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,6 @@
 class PostsController < ApplicationController
   before_action :find_user
 
-
   def index
     @user_posts = @user.posts
   end
@@ -9,7 +8,6 @@ class PostsController < ApplicationController
   def show
     @user_post = @user.posts.find_by(id: params[:id])
     @user = User.find(params[:user_id])
-     @posts = @user.posts.where.not(id: @user_post.id).paginate(page: params[:page], per_page: params[:per_page] || 2)
   end
 
   private
