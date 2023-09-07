433f25ef54e984e1dcb5b053e7bdab79c18a3f60
ProgramKingAbel <abelmorara254@gmail.com>
2023-09-08 00:23:15 +0300

    Implement add comments for a post

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,28 @@
 class CommentsController < ApplicationController
   def new
-    @new_post = Comment.new
-  end 
+     @user = User.find(params[:user_id])
+     @post = Post.find(params[:post_id])
+    @comment = Comment.new
+  end
+
+  def create 
+    @user = current_user
+    @post = Post.find(params[:post_id])
+
+    @new_comment = @post.comments.build(text: comment_params[:text], author_id: @user.id)
+    respond_to do |format|
+      format.html do
+        if @new_comment.save
+          redirect_to user_post_path(current_user, @post), notice: 'Comment created successfully'
+        else
+          redirect_to user_post_path(current_user, @post), alert: 'Cannot create comment'
+        end
+      end
+    end
+  end
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
+
 end
