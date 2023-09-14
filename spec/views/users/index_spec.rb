bf0965ecb71427241f16ac948fdad2a885cd52a4
ProgramKingAbel <abelmorara254@gmail.com>
2023-09-14 21:17:59 +0300

    Add spec test for user#index

--- spec/views/users/index_spec.rb
+++ spec/views/users/index_spec.rb
@@ -1,63 +1,39 @@
 require 'rails_helper'
 
-RSpec.describe 'user index view page', type: :system do
-  let!(:user1) do
-    User.create(
-      name: 'test user1',
-      photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrkfBY9UTdiEHSYCSo7iuM4k1Eyv-u9YwGqQ&usqp=CAU',
-      bio: 'Mic testing one two one two',
-      posts_counter: 1
-    )
-  end
-
-  let!(:user2) do
-    User.create(
-      name: 'testuser2',
-      photo: 'https://lumiere-a.akamaihd.net/v1/images/ct_cinderella_upcportalreskin_20694_f7c876a1.jpeg?region=0,0,330,330',
-      bio: 'Hello doing some tests here',
-      posts_counter: 10
-    )
-  end
-
-  let!(:user3) do
-    User.create(
-      name: 'test user3',
-      photo: 'https://www.rd.com/wp-content/uploads/2020/11/RD-mickey-mouse-GettyImages-1137134597-JVcrop.jpg',
-      bio: 'Test 3 is done here',
-      posts_counter: 20
-    )
-  end
-
-  describe 'user content' do
-    before(:example) do
-      visit users_path
+RSpec.describe 'Users', type: :system do
+  describe 'User index page' do
+    before do
+      allow_any_instance_of(ApplicationController)
+        .to receive(:current_user)
+        .and_return(User.create(name: 'James', photo: 'https://placehold.co/100x100', bio: 'Program tester', posts_counter: 1))
+      @first_user = User.create(name: 'Test', photo: 'https://placehold.co/105x105', bio: 'Fullstack developer')
+      User.create(name: 'Abel', photo: 'https://placehold.co/110x110', bio: 'Fullstack developer')
+      visit root_path
     end
 
-    it 'can see all users names' do
-      expect(page).to have_content(user1.name)
-      expect(page).to have_content(user2.name)
-      expect(page).to have_content(user3.name)
+    it 'Shows the right content' do
+      expect(page).to have_content(@first_user.name)
     end
 
-    it 'can see posts count' do
+    it 'Can see the username of all other users' do
+      expect(page).to have_content('Test')
+      expect(page).to have_content('Abel')
+      expect(page).to have_content('James')
+    end
+
+    it 'Can see the profile picture for each user' do
+      expect(page).to have_css("img[src='https://placehold.co/105x105']")
+      expect(page).to have_xpath("//img[contains(@src,'https://placehold.co/105x105')]")
+      expect(page).to have_css("img[src='https://placehold.co/110x110']")
+    end
+
+    it 'Can see the number of posts written by each user' do
       expect(page).to have_content('Number of posts: 1')
-      expect(page).to have_content('Number of posts: 10')
-      expect(page).to have_content('Number of posts: 20')
     end
 
-    it 'can see the profile picture for each user' do
-      expect(page).to have_css("img[src*='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrkfBY9UTdiEHSYCSo7iuM4k1Eyv-u9YwGqQ']")
-      expect(page).to have_css("img[src*='https://lumiere-a.akamaihd.net/v1/images/ct_cinderella_upcportalreskin_20694_f7c876a1.jpeg?region=0,0,330,330']")
-      expect(page).to have_css("img[src*='https://www.rd.com/wp-content/uploads/2020/11/RD-mickey-mouse-GettyImages-1137134597-JVcrop.jpg']")
+    it 'Redirects to the user show page when a user is clicked' do
+      click_link 'Test'
+      expect(page).to have_current_path(user_path(@first_user))
     end
-  end
-
-  it 'redirects to the user\'s show page' do
-
-    click_link user2.name
-    # visit user_path(user1)
-    expect(page).to have_current_path(user_path(user1))
-    expect(page).to have_content(user1.name)
-    expect(page).to have_content(user1.bio)
   end
 end
