fdf1bee6ea707854ef5b408d5009268f52bdafdb
ProgramKingAbel <abelmorara254@gmail.com>
2023-09-14 13:40:11 +0300

    Generate migration for devise user

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,6 +33,8 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
@@ -58,6 +60,7 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
+  
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
