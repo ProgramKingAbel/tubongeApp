bc8155444352ffb8b30c2a67227df585d6783418
ProgramKingAbel <abelmorara254@gmail.com>
2023-09-15 17:43:08 +0300

    Add endpoint to fetch all posts for a user

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,6 +1,8 @@
-class Api::PostsController < ApplicationController
+class Api::V1::PostsController < ApplicationController
     before_action :find_user
-
+    
+    # http://127.0.0.1:3000/api/v1/users/1/posts
+    
     def index
         @posts = @user.posts
         render json: @posts
--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,12 @@ Rails.application.routes.draw do
 
   root 'users#index'
 
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:index] do
+        resources :posts, only: [:index]
+      end
+    end
+  end
+
 end
