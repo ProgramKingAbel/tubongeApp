8d96721e493097e65984486d1868f685280c0802
Lorenzo <107998561+lorenzoworx@users.noreply.github.com>
2023-09-15 16:57:45 +0100

    ADD api action to list all post comments

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,2 +1,12 @@
 class Api::V1::CommentsController < ApplicationController
+  before_action :find_post
+
+  def index
+      # http://127.0.0.1:3000/api/v1/users/1/posts/102/comments
+      @comments = @post.comments
+      render json: @comments
+    end
+
+  # POST http://127.0.0.1:3000/api/v1/users/1/posts/102/comments
+
 end
