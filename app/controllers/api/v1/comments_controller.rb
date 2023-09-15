e54577f3a3cc69bf5a0492357d030f76831f8754
ProgramKingAbel <abelmorara254@gmail.com>
2023-09-15 21:53:50 +0300

    fix linters

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -2,28 +2,28 @@ class Api::V1::CommentsController < ApplicationController
   before_action :find_post
 
   def index
-      # http://127.0.0.1:3000/api/v1/users/1/posts/102/comments
-      @comments = @post.comments
-      render json: @comments
-    end
+    # http://127.0.0.1:3000/api/v1/users/1/posts/102/comments
+    @comments = @post.comments
+    render json: @comments
+  end
 
   # POST http://127.0.0.1:3000/api/v1/users/1/posts/102/comments
-def create
-  @user = current_user
-  @comment = @post.comments.build(comment_params.merge(author: @user))
+  def create
+    @user = current_user
+    @comment = @post.comments.build(comment_params.merge(author: @user))
 
-  if @comment.save
-    render json: @comment, status: :created
-  else
-    render json: { errors: @comment.errors.full_messages }, status: :unprocessable_entity
+    if @comment.save
+      render json: @comment, status: :created
+    else
+      render json: { errors: @comment.errors.full_messages }, status: :unprocessable_entity
+    end
   end
-end
 
   def find_post
-      @post = Post.find(params[:post_id])
-    end
-  
-    def comment_params
-      params.require(:comment).permit(:text)
-    end
+    @post = Post.find(params[:post_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
 end
