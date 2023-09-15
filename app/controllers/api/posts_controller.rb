e539767c7170c465ac2d8f8f2d83d92671c732ed
ProgramKingAbel <abelmorara254@gmail.com>
2023-09-15 17:26:32 +0300

    Define index action for fetching posts

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,2 +1,13 @@
 class Api::PostsController < ApplicationController
+    before_action :find_user
+
+    def index
+        @posts = @user.posts
+        render json: @posts
+      end
+    private
+
+    def find_user
+      @user = User.includes(posts: :comments).find(params[:user_id])
+    end
 end
