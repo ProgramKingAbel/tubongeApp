c7812ca5268694ece6408179f6e9b70c34142a9b
Lorenzo <107998561+lorenzoworx@users.noreply.github.com>
2023-09-15 17:05:20 +0100

    ADD api action to add a comment to a post

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -8,5 +8,22 @@ class Api::V1::CommentsController < ApplicationController
     end
 
   # POST http://127.0.0.1:3000/api/v1/users/1/posts/102/comments
+def create
+  @user = current_user
+  @comment = @post.comments.build(comment_params.merge(author: @user))
 
+  if @comment.save
+    render json: @comment, status: :created
+  else
+    render json: { errors: @comment.errors.full_messages }, status: :unprocessable_entity
+  end
+end
+
+  def find_post
+      @post = Post.find(params[:post_id])
+    end
+  
+    def comment_params
+      params.require(:comment).permit(:text)
+    end
 end
